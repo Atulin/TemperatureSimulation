aa73e88b899152aefb2a68c6b4f8c93b5e4a4c5f
Atulin <11233299+Atulin@users.noreply.github.com>
2024-04-25 00:07:29 +0200

    Added plots

---
+++ move.ps1
@@ -0,0 +1,6 @@
+﻿New-Item -ItemType Directory -Path "./Plots"
+
+$files = Get-ChildItem "./TemperatureSimulation/bin/Debug/net8.0/plot_*"
+foreach ($f in $files) {
+    Move-Item -Path $f -Destination "./Plots"
+}
